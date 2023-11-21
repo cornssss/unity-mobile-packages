cca6be1f7eefbbd8861328fbee780105073b4162
“Jerald <dlarejcasulla12@gmail.com>
2023-11-21 10:03:40 +0800

    init

--- PinchZoom.cs
+++ PinchZoom.cs
@@ -1,6 +1,34 @@
 
+/// <summary>
+/// What happens here?
+/// 
+/// On InitPinch, initial values are stored like position of 2 touches so that
+/// the next positions fr each frame has reference to it and make a scale of 
+/// zoom.
+/// 
+/// StartPinch is called every frame where two touches drags on the screen.
+/// The method generates a pinch scale value to scale the current camera's 
+/// orthographic size.
+/// 
+/// Most likely, you can use pinch scale variable to change the value of 
+/// orthographic size of camera or the distance of it in single axis.
+/// </summary>
+
+  //To use PinchZoom, 
+
+  //First, create a UI Panel to the hirarchy,
+
+  //Second, add a trigger events component to the UI Panel
+
+  //Add the following events: OnBeginDrag and OnDrag
+
+  //Lastly,drop the InitPinch to OnBeginDrag and drop StartPinch to OnDrag;
+
+
 using UnityEngine;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+
+//NOTE: This api uses a EnhancedTouch of New Input System.
 
 public class CanvasPinch : MonoBehaviour
 {
@@ -12,6 +40,7 @@ public class CanvasPinch : MonoBehaviour
   private Controller controller;
 
   private void Awake() {
+    //This is not required, you can reference pinchscale to your own scripts
     controller = FindObjectOfType<Controller>();
   }
 
@@ -29,11 +58,12 @@ public class CanvasPinch : MonoBehaviour
           initialDistance = Vector2.Distance(initialTouch1Position, initialTouch2Position);
           currentDistance = Vector2.Distance(currentTouch1Position, currentTouch2Position);
 
-          // Calculate the pinch scale
+          // Calculate the initial pinch scale
           pinchScale = currentDistance / initialDistance;
           multiplier = pinchScale * controller.Zoom;
       }
   }
+  
   public void StartPinch()
   {
     if (Touch.activeTouches.Count != 2) return;
